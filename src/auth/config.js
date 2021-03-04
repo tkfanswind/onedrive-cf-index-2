b9534b734b3cad04ca7ee101f67b085ed17fc4fc
tkfanswind <55653816+tkfanswind@users.noreply.github.com>
2021-03-04 21:39:06 +0800

    Update config.js

--- src/auth/config.js
+++ src/auth/config.js
@@ -15,7 +15,7 @@ export const PASS = AUTH_PASSWORD
 // export const PASS = ''
 
 export const DISABLE_PATHS = ['/favicon.ico', '/robots.txt']
-export const ENABLE_PATHS = ['Films/007', 'Films/John Wick', 'NEW']
+export const ENABLE_PATHS = ['007', 'John Wick', 'NEW']
 
 /**
  * RegExp for basic auth credentials
