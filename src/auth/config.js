7c20c21589c3c40c5f39bad6f5c4b6eadb42924d
tkfanswind <55653816+tkfanswind@users.noreply.github.com>
2021-03-06 16:30:14 +0800

    Update config.js

--- src/auth/config.js
+++ src/auth/config.js
@@ -15,7 +15,7 @@ export const PASS = AUTH_PASSWORD
 // export const PASS = ''
 
 export const DISABLE_PATHS = ['/favicon.ico', '/robots.txt']
-export const ENABLE_PATHS = ['/Private folder/Private folder', '/Private folder/Private folder2']
+export const ENABLE_PATHS = ['/Private folder/Private folder', '/Private folder/Private folder3']
 
 /**
  * RegExp for basic auth credentials
