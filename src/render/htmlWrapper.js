34c3309e9cd0bedfc8edbc77fb963158ed13b91e
tkfanswind <55653816+tkfanswind@users.noreply.github.com>
2021-03-07 10:22:47 +0800

    Update htmlWrapper.js

--- src/render/htmlWrapper.js
+++ src/render/htmlWrapper.js
@@ -32,7 +32,7 @@ export function renderHTML(body, pLink, pIdx) {
       <meta charset="utf-8" />
       <meta http-equiv="x-ua-compatible" content="ie=edge, chrome=1" />
       <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-      <title>🍎 tkfans OneDrive</title>
+      <title>tkfans OneDrive</title>
       <link rel="shortcut icon" type="image/png" sizes="16x16" href="${favicon}" />
       <link href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.13.1/css/all.min.css" rel="stylesheet">
       <link href="https://cdn.jsdelivr.net/gh/tkfanswind/onedrive-cf-index-1@${COMMIT_HASH}/themes/spencer.css" rel="stylesheet">
