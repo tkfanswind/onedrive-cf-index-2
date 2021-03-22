7762362b409f30dc94e6da601c19e8ed0c64a258
tkfanswind <55653816+tkfanswind@users.noreply.github.com>
2021-03-22 16:59:30 +0800

    Update folderView.js

--- src/folderView.js
+++ src/folderView.js
@@ -54,15 +54,14 @@ export async function renderFolderView(items, path, request) {
                     <p>This is tkfans OneDrive public directory listing.  Reach me at:  tkfanswind@gmail.com.</p>
                     <p><a href="https://od.tkfans.tk/">OneDrive OLAINDEX</a></p>
                     <p><a href="https://onedrive.tkfans.tk/">OneDrive FODI</a></p>
-
-             ### 📁 NEW
-
-
-                        <meta name="referrer" content="never">
+         
+             <h1>### 📁 NEW</h1>                        
+                              <meta name="referrer" content="never">
             <img src="https://img9.doubanio.com/view/photo/l/public/p2634360594.jpg" width="60%" height="60%">
             <img src="https://img9.doubanio.com/view/photo/l/public/p2630139764.jpg" width="60%" height="60%">
             <img src="https://img9.doubanio.com/view/photo/l/public/p2632487996.jpg" width="60%" height="60%">
             <img src="https://img3.doubanio.com/view/photo/l/public/p2632712700.jpg" width="60%" height="60%">
+            <img src="https://www.wowow.co.jp/drama/original/influence/img/mv.jpg">
             <img src="https://img9.doubanio.com/view/photo/l/public/p2630644695.jpg">
 
                   </div>`
